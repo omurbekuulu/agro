65200d88491a730435cfffce5b7fe92a5dee57c7
Marlen <marlenomurbekuulu66@gmail.com>
2024-11-14 06:51:27 +0600

    remove app bars

--- lib/presentation/pages/expense_page.dart
+++ lib/presentation/pages/expense_page.dart
@@ -19,72 +19,81 @@ class _ExpensePageState extends State<ExpensePage> {
     final typography = Theme.of(context).appTypography;
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back_ios_new_outlined),
-        ),
-        title: Text(
-          'Кошумча чыгаша',
-          style: typography.h2.bold,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 14),
-              Text(
-                'Сумма',
-                style: typography.p1.medium.copyWith(
-                  color: colors.secondary1,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Кошумча чыгаша',
+                      style: typography.h2.bold,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        'Артка кайтуу',
+                        style: typography.p1.bold.copyWith(color: Colors.black),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              const SizedBox(height: 4),
-              customTextFormField(context, hintText: '0 с'),
-              const SizedBox(height: 24),
-              Text(
-                'Комментарий (Аты)',
-                style: typography.p1.medium.copyWith(
-                  color: colors.secondary1,
-                ),
-              ),
-              const SizedBox(height: 4),
-              customTextFormField(context, hintText: 'Сумма'),
-              const SizedBox(height: 24),
-              Text(
-                'Саны',
-                style: typography.p1.medium.copyWith(
-                  color: colors.secondary1,
-                ),
-              ),
-              customTextFormField(context, hintText: '0'),
-              const SizedBox(height: 4),
-              const Spacer(),
-              Row(
-                children: [
-                  const Spacer(),
-                  SvgPicture.asset(
-                    'assets/logo.svg',
-                    width: 57,
+                const SizedBox(height: 14),
+                Text(
+                  'Сумма',
+                  style: typography.p1.medium.copyWith(
+                    color: colors.secondary1,
                   ),
-                ],
-              ),
-              const SizedBox(height: 24),
-              FilledButton(
-                onPressed: () {},
-                child: Text(
-                  'Кошуу',
-                  style: typography.h3.bold,
                 ),
-              ),
-              const SizedBox(height: 12),
-            ],
+                const SizedBox(height: 4),
+                customTextFormField(context, hintText: '0 с'),
+                const SizedBox(height: 24),
+                Text(
+                  'Комментарий (Аты)',
+                  style: typography.p1.medium.copyWith(
+                    color: colors.secondary1,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                customTextFormField(context, hintText: 'Сумма'),
+                const SizedBox(height: 24),
+                Text(
+                  'Саны',
+                  style: typography.p1.medium.copyWith(
+                    color: colors.secondary1,
+                  ),
+                ),
+                customTextFormField(context, hintText: '0'),
+                const SizedBox(height: 4),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Spacer(),
+                    SvgPicture.asset(
+                      'assets/logo.svg',
+                      width: 57,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                FilledButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Кошуу',
+                    style: typography.h3.bold,
+                  ),
+                ),
+                const SizedBox(height: 12),
+              ],
+            ),
           ),
         ),
       ),
